bf457e550671ae724a6dc983596d8af274cd7032
romanitwork <workromanmail@gmail.com>
2020-01-29 14:37:26 +0200

    done task-1,2,3,4

---
+++ js/task-3.js
@@ -0,0 +1,36 @@
+"use strict";
+
+class Storage {
+  constructor(items) {
+    this.items = items;
+  }
+  getItems() {
+    return this.items;
+  }
+  addItem(item) {
+    this.items.push(item);
+  }
+  removeItem(item) {
+    for (let index of this.items) {
+      if (index === item) {
+        this.items.splice(this.items.indexOf(index), 1);
+      }
+    }
+  }
+}
+
+const storage = new Storage([
+  "Нанитоиды",
+  "Пролонгер",
+  "Железные жупи",
+  "Антигравитатор"
+]);
+
+const items = storage.getItems();
+console.table(items); // [ "Нанитоиды", "Пролонгер", "Железные жупи", "Антигравитатор" ]
+
+storage.addItem("Дроид");
+console.table(storage.items); // [ "Нанитоиды", "Пролонгер", "Железные жупи", "Антигравитатор", "Дроид" ]
+
+storage.removeItem("Пролонгер");
+console.table(storage.items); // [ "Нанитоиды", "Железные жупи", "Антигравитатор", "Дроид" ]
